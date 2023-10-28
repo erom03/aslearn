f6a0be291113b042851815a5c205f6f6d1d83e3e
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-28 19:46:05 -0400

    Change CheckForUser.js to CheckForUser.ts Also add type safty to user object

--- app/context/CheckForUser.ts
+++ app/context/CheckForUser.ts
@@ -1,8 +1,9 @@
 import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebase"
+import { User } from "firebase/auth";
 
 // Function that takes in a user object and checks if they exist in the database
-export const CheckUser = async (user) => {
+export const CheckUser = async (user: User) => {
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
 
