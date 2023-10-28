7f13b3eea759804872bf1c3d522812a5d848a826
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-28 02:50:54 -0400

    Delete whitespace

--- app/firebase.js
+++ app/firebase.js
@@ -5,7 +5,6 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
